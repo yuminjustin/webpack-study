e6653f83330651fc55ba04d0d66ff64c957b13f3
justin lee <yuminjustin@163.com>
2017-09-07 17:19:14 +0800

    Create webpack.dev.conf.js

---
+++ build/webpack.dev.conf.js
@@ -0,0 +1,50 @@
+var utils = require('./utils')
+var webpack = require('webpack')
+var config = require('../config')
+var merge = require('webpack-merge')
+var baseWebpackConfig = require('./webpack.base.conf')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+// add hot-reload related code to entry chunks
+Object.keys(baseWebpackConfig.entry).forEach(function (name) {
+  baseWebpackConfig.entry[name] = ['./build/dev-client'].concat(baseWebpackConfig.entry[name])
+})
+
+module.exports = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
+  },
+  // cheap-module-eval-source-map is faster for development
+  devtool: '#cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': config.dev.env
+    }),
+    // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // https://github.com/ampedandwired/html-webpack-plugin
+    
+    // 多入口配置
+    
+    //入口一
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      status: 0,
+      inject: true,
+      chunks: ['app'],
+    }),
+     //入口二
+    new HtmlWebpackPlugin({
+      filename: 'app2.html',
+      template: 'app2.html',
+      status: 1,
+      inject: true,
+      chunks: ['app2'],
+    }),
+    
+    new FriendlyErrorsPlugin()
+  ]
+})
